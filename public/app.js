00353e9b20e4c6b1bd73a82cc87cdb72f707c73d
Enerhim2 <himsharma9122006@gmail.com>
2021-10-10 15:46:22 +0530

    Home Screen

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 const auth = firebase.auth()
+const db = firebase.firestore()
 
+// Get Elements
 const whenSignedIn = document.getElementById("signedIn")
 const whenSignedOut = document.getElementById("signedOut")
 
@@ -8,8 +10,11 @@ const signOutButton = document.getElementById("signOutBtn")
 
 const provider = new firebase.auth.GoogleAuthProvider()
 
+// Google Sign in Button Events
 signInButton.onclick = () => auth.signInWithPopup(provider)
 signOutButton.onclick = () => auth.signOut()
+
+// On Signin or Signout
 
 auth.onAuthStateChanged(user => {
     if (user) {
@@ -23,4 +28,30 @@ auth.onAuthStateChanged(user => {
         signInButton.hidden = false
         signOutButton.hidden = true
     }
+})
+
+// Get Firestore Data
+
+var tutorialsRef = db.collection("tutorials").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        document.querySelector(".container").innerHTML += 
+        `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title"> ${doc.get("Title")} </h5>
+            <p class="card-text">${doc.get("Description")}</p>
+                <div class="author-container d-flex inline align-items-center">
+                    <a href="#" class="btn btn-primary">Read  </a>
+                    <img src="${doc.get("AuthorProfile")}" alt="Avatar" class="author-pfp ms-4" style="
+                        vertical-align: middle;
+                        width: 35px;
+                        height: 35px;
+                        border-radius: 50%;
+                    ">
+                    <p class="mb-0 ms-1">${doc.get("AuthorName")}</p>
+                </div>
+            </div>
+        </div>
+        `
+    })
 })
